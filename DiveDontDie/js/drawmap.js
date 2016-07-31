856880fb154a192258c40a66a503fb579a55beec
Marcus <20947214@student.uwa.edu.au>
2016-07-31 13:59:10 +0800

    Updated heatmap scale

--- DiveDontDie/js/drawmap.js
+++ DiveDontDie/js/drawmap.js
@@ -1,118 +1,58 @@
-// This example requires the Visualization library. Include the libraries=visualization
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
 
 var map, heatmap;
 
-var csv = "EXPORT_Detection20160727";
+var csv1 = "EXPORT_Detection20160727";
+var csv2 = "EXPORT_Sighting20160727";
+
+var points = [];
 
 function initMap() {
 
-	$.get(csv + ".csv", function(csvFile) {
+	$.get(csv1 + ".csv", function(csvFile1) {
+		points = parseCSVData(csvFile1);
 	
 	//$("#infoOne").text(parseCSVData(csvFile));
 	
-  map1 = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: {lat: -32.0231, lng: 115.4765},
-    mapTypeId: 'roadmap',
-    
-    styles: [
-    	{
-    	featureType: 'all',
-    	stylers: [
-  			{ visibility: 'off' }
-			]
-		},
-		{
-    	featureType: 'water',
-    	stylers: [
-  			{ visibility: 'on' }
-			]
-		}
-		]
-  });
-  
-  map2 = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: {lat: -32.0231, lng: 115.4765},
-    mapTypeId: 'roadmap',
-    
-    styles: [
-    	{
-    	featureType: 'all',
-    	stylers: [
-  			{ visibility: 'on' }
-			]
-		},
-		{
-    	featureType: 'water',
-    	elementType: 'geometry',
-    	stylers: [
-  			{ visibility: 'off' }
-			]
-		},
-		{
-        featureType: "landscape.natural",
-        elementType: "geometry",
-        stylers: [
-        	{ visibility: "off" }
-              ]
-          }
-		]
-  });
+		$.get(csv2 + ".csv", function(csvFile2) {
 
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: parseCSVData(csvFile),
-    map: map1
-  });
-  
-  heatmap.set('radius', 100);
-  
+			points = points.concat(parseCSVData(csvFile2));
+			
+		  map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 9,
+			center: {lat: -32.9309, lng: 115.7818},
+			mapTypeId: 'roadmap',
+			minZoom: 9,
+			maxZoom: 14
+		  });
+
+
+		  heatmap = new google.maps.visualization.HeatmapLayer({
+			data: points,
+			map: map
+		  });
+		  
+				heatmap.setOptions({
+						dissipating: true,
+						radius: 70,
+						//dissipating: false
+				});
+		}, "text");
 	}, "text");
 }
 
-function toggleHeatmap() {
-  heatmap.setMap(heatmap.getMap() ? null : map);
-}
 
-function changeGradient() {
-  var gradient = [
-    'rgba(0, 255, 255, 0)',
-    'rgba(0, 255, 255, 1)',
-    'rgba(0, 191, 255, 1)',
-    'rgba(0, 127, 255, 1)',
-    'rgba(0, 63, 255, 1)',
-    'rgba(0, 0, 255, 1)',
-    'rgba(0, 0, 223, 1)',
-    'rgba(0, 0, 191, 1)',
-    'rgba(0, 0, 159, 1)',
-    'rgba(0, 0, 127, 1)',
-    'rgba(63, 0, 91, 1)',
-    'rgba(127, 0, 63, 1)',
-    'rgba(191, 0, 31, 1)',
-    'rgba(255, 0, 0, 1)'
-  ]
-  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-}
 
-function changeRadius() {
-  heatmap.set('radius', heatmap.get('radius') ? null : 20);
-}
-
-function changeOpacity() {
-  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-}
 
 
 
 function parseCSVData(csvFile) {
 
 	var data = [];
-
+	
 	//Split csv file into array of rows
 	var lines = csvFile.split("\n");
 	var today = new Date();
+	var date;
 
 	//Iterate through each row
 	$.each(lines, function (lineNumber, line) {
@@ -123,8 +63,10 @@ function parseCSVData(csvFile) {
 		//Push to data array if not empty
 		if (fields != "") {
 			
+			if (fields.length<9) {
+				date = Date.parse(fields[fields.length - 4]);
+			} else date = Date.parse(fields[fields.length - 7]);
 			
-			var date = Date.parse(fields[fields.length - 4]);
 			
 			
 			var timeDif = today - date;
@@ -132,28 +74,50 @@ function parseCSVData(csvFile) {
 			var lo = parseFloat(fields[fields.length - 2]);
 			var la = parseFloat(fields[fields.length - 1]);
 		
-			var dist = timeDif*0.000000000075;
+			//var dist = timeDif*0.0000000000075;
 		
-			var wei = 1/(dist*4);
-			
+			var wei = (timeDif/2592000000)/2;
+			/*
 			var lap = la - -dist;
 			var lan = la - dist;
 			var lop = lo - -dist;
 			var lon = lo - dist;
+			*/
+			var rad = 0.0045;
 			
-			$("#infoTwo").append(dist + "," + wei + ",");
+			$("#infoTwo").append(wei + "," + timeDif + ",");
+			
+			var dist = parseInt(wei*9);
 		
+			for (i=1; i<dist; i++) {
+				if (lo < 115.7547756) {
+				for (j=0; j<i+1; j++){
+					data.push({location:new google.maps.LatLng(la - i*rad,lo - j*rad), weight: wei});
+					data.push({location:new google.maps.LatLng(la - -i*rad,lo - j*rad), weight: wei});
+				}
+				for (k=1; k<2*i-1; k++) {
+					data.push({location:new google.maps.LatLng(la - k*rad,lo - i*rad), weight: wei});
+					data.push({location:new google.maps.LatLng(la - -k*rad,lo - i*rad), weight: wei});
+				}
+				}
+				data.push({location:new google.maps.LatLng(la,lo), weight: wei});
+				data.push({location:new google.maps.LatLng(la,lo - i*rad), weight: wei});
+			}
+		
+			/*
 			data.push({location:new google.maps.LatLng(la,lo), weight: wei});
 			data.push({location:new google.maps.LatLng(lap,lo), weight: wei});
-			data.push({location:new google.maps.LatLng(la,lop), weight: wei});
+			//data.push({location:new google.maps.LatLng(la,lop), weight: wei});
 			data.push({location:new google.maps.LatLng(lan,lo), weight: wei});
 			data.push({location:new google.maps.LatLng(la,lon), weight: wei});
-			data.push({location:new google.maps.LatLng(lap,lop), weight: wei});
-			data.push({location:new google.maps.LatLng(lan,lop), weight: wei});
+			//data.push({location:new google.maps.LatLng(lap,lop), weight: wei});
+			//data.push({location:new google.maps.LatLng(lan,lop), weight: wei});
 			data.push({location:new google.maps.LatLng(lap,lon), weight: wei});
 			data.push({location:new google.maps.LatLng(lan,lon), weight: wei});
+			*/
 			
 		}
+		if (lineNumber == 30) return false;
 	});
 	return data;
 }
