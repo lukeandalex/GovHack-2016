3e5f3a3e6981650a33ca18f5387c78da318a9b40
Luke <luke.martin311@gmail.com>
2016-07-31 19:04:05 +0800

    Merge branch 'master' of https://github.com/lukeandalex/GovHack-2016
    # Conflicts:
    #	DiveDontDie/js/drawmap.js

---
+++ DiveDontDie/js/drawmap.js
@@ -0,0 +1,151 @@
+
+var map, heatmap;
+
+var csv1 = "EXPORT_Detection20160727";
+var csv2 = "EXPORT_Sighting20160727";
+
+var points = [];
+var lat, lng;
+
+var marker;
+
+
+
+
+function initMap() {
+
+	$.get(csv1 + ".csv", function(csvFile1) {
+		points = parseCSVData(csvFile1);
+	
+	//$("#infoOne").text(parseCSVData(csvFile));
+	
+		$.get(csv2 + ".csv", function(csvFile2) {
+
+			points = points.concat(parseCSVData(csvFile2));
+			
+		  map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 9,
+			center: {lat: -32.9309, lng: 115.7818},
+			mapTypeId: 'roadmap',
+			minZoom: 9,
+			maxZoom: 14
+		  });
+		 
+		GetMarineWeather(-32.9309, 115.7818);
+		
+		google.maps.event.addListener(map, 'click', function( event ){
+			if (marker) {
+				marker.setMap(null);
+			}
+			lat = event.latLng.lat();
+			lng = event.latLng.lng(); 
+			placeMarker(event.latLng);
+
+			
+		});
+
+
+		  heatmap = new google.maps.visualization.HeatmapLayer({
+			data: points,
+			map: map
+		  });
+		  
+				heatmap.setOptions({
+						dissipating: true,
+						radius: 70,
+						//dissipating: false
+				});
+		}, "text");
+	}, "text");
+}
+
+
+
+function placeMarker(location) {
+    marker = new google.maps.Marker({
+        position: location, 
+        map: map
+    });
+	GetMarineWeather(lat, lng);
+}
+
+
+function parseCSVData(csvFile) {
+
+	var data = [];
+	
+	//Split csv file into array of rows
+	var lines = csvFile.split("\n");
+	var today = new Date();
+	var date;
+
+	//Iterate through each row
+	$.each(lines, function (lineNumber, line) {
+	
+		//Split line into array of columns
+		var fields = line.split(",");
+	
+		//Push to data array if not empty
+		if (fields != "") {
+			
+			if (fields.length<9) {
+				date = Date.parse(fields[fields.length - 4]);
+			} else date = Date.parse(fields[fields.length - 7]);
+			
+			
+			
+			var timeDif = today - date;
+			
+			var lo = parseFloat(fields[fields.length - 2]);
+			var la = parseFloat(fields[fields.length - 1]);
+		
+			//var dist = timeDif*0.0000000000075;
+		
+			var wei = (timeDif/2592000000)/3;
+			/*
+			var lap = la - -dist;
+			var lan = la - dist;
+			var lop = lo - -dist;
+			var lon = lo - dist;
+			*/
+			var rad = 0.0045;
+			
+			
+			var dist = parseInt(wei*21);
+			
+			//var wei = 1/timeScaled;
+		
+			//$("#infoTwo").append(wei + "," + dist + ",");
+			
+			for (i=1; i<dist; i++) {
+				if (lo < 115.7547756) {
+				for (j=0; j<i+1; j++){
+					data.push({location:new google.maps.LatLng(la - i*rad,lo - j*rad), weight: wei});
+					data.push({location:new google.maps.LatLng(la - -i*rad,lo - j*rad), weight: wei});
+				}
+				for (k=1; k<2*i-1; k++) {
+					data.push({location:new google.maps.LatLng(la - k*rad,lo - i*rad), weight: wei});
+					data.push({location:new google.maps.LatLng(la - -k*rad,lo - i*rad), weight: wei});
+				}
+				}
+				data.push({location:new google.maps.LatLng(la,lo), weight: wei*10});
+				data.push({location:new google.maps.LatLng(la,lo - i*rad), weight: wei});
+			}
+		
+			/*
+			data.push({location:new google.maps.LatLng(la,lo), weight: wei});
+			data.push({location:new google.maps.LatLng(lap,lo), weight: wei});
+			//data.push({location:new google.maps.LatLng(la,lop), weight: wei});
+			data.push({location:new google.maps.LatLng(lan,lo), weight: wei});
+			data.push({location:new google.maps.LatLng(la,lon), weight: wei});
+			//data.push({location:new google.maps.LatLng(lap,lop), weight: wei});
+			//data.push({location:new google.maps.LatLng(lan,lop), weight: wei});
+			data.push({location:new google.maps.LatLng(lap,lon), weight: wei});
+			data.push({location:new google.maps.LatLng(lan,lon), weight: wei});
+			*/
+			
+		}
+		if (lineNumber == 30) return false;
+	});
+	return data;
+}
---
+++ DiveDontDie/plugins/freeAPI.js
@@ -0,0 +1,151 @@
+﻿var _FreeApiBaseURL = 'http://api.worldweatheronline.com/premium/v1/';
+/*
+    Please change the FreeAPIKey to your own. 
+    These keys have been provided for testing only.
+    If you don't have one, then register now: http://developer.worldweatheronline.com/member/register    
+*/
+var _FreeApiKey = '37d91fdb2ee14af0a6395909163107';
+
+// -------------------------------------------
+
+function JSONP_LocalWeather(input) {
+    var url = _FreeApiBaseURL + 'weather.ashx?q=' + input.query + '&format=' + input.format + '&extra=' + input.extra + '&num_of_days=' + input.num_of_days + '&date=' + input.date + '&fx=' + input.fx + '&cc=' + input.cc + '&includelocation=' + input.includelocation + '&show_comments=' + input.show_comments + '&key=' + _FreeApiKey;
+
+    jsonP(url, input.callback);
+}
+
+function JSONP_SearchLocation(input) {
+    var url = _FreeApiBaseURL + "search.ashx?q=" + input.query + "&format=" + input.format + "&timezone=" + input.timezone + "&popular=" + input.popular + "&num_of_results=" + input.num_of_results + "&key=" + _FreeApiKey;
+
+    jsonP(url, input.callback);
+}
+
+function JSONP_TimeZone(input) {
+    var url = _FreeApiBaseURL + "tz.ashx?q=" + input.query + "&format=" + input.format + "&key=" + _FreeApiKey;
+
+    jsonP(url, input.callback);
+}
+
+function JSONP_MarineWeather(input) {
+    var url = _FreeApiBaseURL + "marine.ashx?q=" + input.query + "&format=" + input.format + "&fx=" + input.fx + "&key=" + _FreeApiKey + "&tide=yes";
+
+    jsonP(url, input.callback);
+
+}
+
+// -------------------------------------------
+
+// Helper Method
+function jsonP(url, callback) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        contentType: "application/json",
+        jsonpCallback: callback,
+        dataType: 'jsonp',
+        success: function (json) {
+            console.dir('success');
+			
+        },
+        error: function (e) {
+            console.log(e.message);
+        }
+    });
+}
+
+var dateOutput = '';
+var swellOutput = '';
+var windOutput = '';
+var windDirectionOutput = '';
+var tideOutput = '';
+var waterTemp = '';
+var conditions;
+var dateToString;
+var dateToPass;
+var cloudCover;
+
+
+
+
+
+
+
+
+function GetMarineWeather(late, lnge) {
+	
+	
+    var marineWeatherInput = {
+		query: late+","+lnge,
+        format: 'JSON',
+        fx: '',
+        callback: 'MarineWeatherCallback'
+    };
+	//marineWeatherInput.query = 
+    JSONP_MarineWeather(marineWeatherInput);
+    //e.preventDefault();
+	
+}
+
+
+
+
+function MarineWeatherCallback(marineWeather) {
+	
+    var allDataToday = marineWeather.data.weather[0];
+    dateOutput = allDataToday.date;
+    swellOutput = allDataToday.hourly[2].swellHeight_m + "M";
+    windDirectionOutput = allDataToday.hourly[2].winddir16Point;
+    windOutput = allDataToday.hourly[2].windspeedKmph + "KM/h  " + windDirectionOutput;
+    tideOutput = allDataToday.tides[0].tide_data[0].tideHeight_mt + "M";
+    waterTemp = allDataToday.hourly[2].waterTemp_C + "°";
+    cloudCover = allDataToday.hourly[2].cloudcover + "%";
+
+    //dateToPass = Date.parse(dateOutput);
+	var today = new Date();
+	var weekday = new Array(7);
+	weekday[0]=  "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+	
+	var month = new Array(12);
+	month[0]=  "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+
+
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+
+	var n = weekday[today.getDay()];
+	var m = month[mm-1];
+
+
+	today = n + " " + dd + " " + m;
+
+
+
+    //$('#swell').empty();
+
+    $('#conditionsDate').html(today);
+    $('#swell').html(swellOutput);
+    $('#wind').html(windOutput);
+    $('#tide').html(tideOutput);
+    $('#waterTemp').html(waterTemp);
+    $('#season').html(cloudCover);
+}
+
+
